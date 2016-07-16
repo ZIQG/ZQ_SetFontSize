bc1041a176ab65d133ff5f54b3309a11b5e18bd2
ZIQG <365934970@qq.com>
2016-07-16 15:48:21 +0800

    Initial Commit

---
+++ ZQ_SetFontSize/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZQ_SetFontSize
+//
+//  Created by hzq on 16/7/16.
+//  Copyright © 2016年 hzq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZQ_SetFontSize/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZQ_SetFontSize
+//
+//  Created by hzq on 16/7/16.
+//  Copyright © 2016年 hzq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
